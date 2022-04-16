2e3020c17a5761ad07535d0704ac1a25869b4dd3
Partho Rahman <partho.rahman@sjinnovation.com>
2022-04-17 03:21:19 +0600

    total + tax + grand total done

--- app.js
+++ app.js
@@ -18,9 +18,29 @@ function handlerProductCount(product, isIncrease) {
         productTotal = productNewCount * 59;
     }
     document.getElementById(product + '-total').innerText = "$ " + productTotal;
+   
+    calculateTotal();
  }
 
+ function calculateTotal() {
+     const phoneCount = getInputValue('phone');
+     const caseCount = getInputValue('case');
 
+     const totalPrice = phoneCount * 1219 + caseCount * 59;
+     document.getElementById('total-price').innerText = '$' + totalPrice;
+
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById('tax-amount').innerText = '$' + tax;
+ 
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+ function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+ }
 // third Pre JS to start: 
 
 // function handlerPhoneCount(isIncrease) {
