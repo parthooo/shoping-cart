7b8918bf23c193f0e1486bfb29f3260820cacc86
Partho Rahman <partho.rahman@sjinnovation.com>
2022-04-17 02:38:28 +0600

    phone & case both are inside single function

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
-function handlerPhoneCount(isIncrease) {
-    const phoneInput = document.getElementById('phone-count');
-    const phoneCount = parseInt(phoneInput.value);
-    let phoneNewCount = phoneCount;
+function handlerProductCount(product, isIncrease) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
     if (isIncrease == true) {
-        phoneNewCount = phoneCount+1;
+        productNewCount = productCount+1;
     }
-    if(isIncrease == false && phoneCount > 0){
-        phoneNewCount = phoneCount-1;
+    if(isIncrease == false && productCount > 0){
+        productNewCount = productCount-1;
     }
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById('phone-total').innerText = "$ " + phoneTotal;
+    productInput.value = productNewCount;
+    // const caseTotal = productNewCount * 59;
+    let productTotal = 0;
+    if(product == 'phone'){
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case'){
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + '-total').innerText = "$ " + productTotal;
  }
 
 
-function handlerProductCount(isIncrease) {
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
-    if (isIncrease == true) {
-       caseNewCount = caseCount+1;
-    }
-    if(isIncrease == false && caseCount > 0){
-       caseNewCount = caseCount-1;
-    }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerText = "$ " + caseTotal;
- }
+// third Pre JS to start: 
+
+// function handlerPhoneCount(isIncrease) {
+//     const phoneInput = document.getElementById('phone-count');
+//     const phoneCount = parseInt(phoneInput.value);
+//     let phoneNewCount = phoneCount;
+//     if (isIncrease == true) {
+//         phoneNewCount = phoneCount+1;
+//     }
+//     if(isIncrease == false && phoneCount > 0){
+//         phoneNewCount = phoneCount-1;
+//     }
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById('phone-total').innerText = "$ " + phoneTotal;
+//  }
 
 
+// function handlerProductCount(isIncrease) {
+//     const caseInput = document.getElementById('case-count');
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+//     if (isIncrease == true) {
+//        caseNewCount = caseCount+1;
+//     }
+//     if(isIncrease == false && caseCount > 0){
+//        caseNewCount = caseCount-1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById('case-total').innerText = "$ " + caseTotal;
+//  }
+
+//////////////////////////////////////////////////////
 
 
 
@@ -60,7 +85,7 @@ function handlerProductCount(isIncrease) {
 //  }
 
 
-
+//////////////////////////////////////////////////////
 
 
  // First Pre JS to start: 
@@ -82,3 +107,5 @@ function handlerProductCount(isIncrease) {
  //    const caseTotal = caseNewCount * 59;
  //    document.getElementById('case-total').innerText = "$ " + caseTotal;
  // })
+
+ //////////////////////////////////////////////////////
